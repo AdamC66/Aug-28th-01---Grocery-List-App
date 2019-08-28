27ea03996c8232574d9c6f2afa394b50b19046f4
acote66 <adam.cote66@gmail.com>
2019-08-28 18:38:02 -0400

    got plus and minus buttons working

--- src/listItem.js
+++ src/listItem.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
-const ListItem = ({category, quantity, item}) => {
-    
+class ListItem extends React.Component{
+    state = {
+        quantity: 1,
+        category:this.props.category,
+        item: this.props.item
+    }
+    updateValueDown = (newValue) =>{
+        let down = this.state.quantity
+        if(this.state.quantity!= 0) down = this.state.quantity -= 1
+        this.setState({quantity: down})
+    }
+    updateValueUp = (newValue) =>{
+        let up = this.state.quantity += 1
+        this.setState({quantity: up})        
+    }
+    render(){
     return(
-    <li className={category}>
-        <button>-</button>
-        <span>{quantity} {item}</span>
-        <button>+</button>
+    <li className={this.state.category}>
+        <button onClick={ () => { this.updateValueDown() } }>-</button>
+        <span>{this.state.quantity} {this.state.item}</span>
+        <button onClick={ () => { this.updateValueUp() } } >+</button>
     </li>
     )
-
+    }
 }
 
 export default ListItem
--- src/shoppingList.js
+++ src/shoppingList.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ListItem from "./listItem";
 
-const ShoppingList = () =>{
+const ShoppingList = () => {
+    
+
     return(
         <ul className="shoppinglist" id = "ShoppingList">
-            <ListItem category={'meat'} quantity={3} item={'Steak'}/>
-            <ListItem category={'prod'} quantity={4} item={'Apples'}/>
-            <ListItem category={'dairy'} quantity={1} item={'Milk (1L, 2%)'}/>
-            <ListItem category={'bakery'} quantity={2} item={'Baguettes'}/>
+            <ListItem category={'meat'}  item={'Steak'} />
+            <ListItem category={'prod'}  item={'Apples'} />
+            <ListItem category={'dairy'}  item={'Milk (1L, 2%)'} />
+            <ListItem category={'bakery'}  item={'Baguettes'} />
         </ul>
     )
-
+    
 }
 
 export default ShoppingList;
